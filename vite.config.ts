fc4916fa00818f2571d9795744590eb6eb5db3da
Kanishka Pandey <kanishkapandey@Kanishkas-MacBook-Air.local>
2026-03-30 23:59:32 +0530

    fix: resolve Vercel deployment base url issue

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/",
+  // Use root for Vercel, and /portfolio/ for GitHub Pages
+  base: process.env.VERCEL ? "/" : "/portfolio/",
 });
